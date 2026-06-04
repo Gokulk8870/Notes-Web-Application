e72247064de50c246e92af9c523bc28abeca7514
Gokul K <gokulgokul4457@gmail.com>
2026-06-04 22:08:34 +0530

    fix:storing array on notes

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ var popupbox=document.querySelector(".popup-box");
 var addpopup=document.getElementById("add-popup");
 var addnote=document.getElementById("add-note");
 let notes=[];
+
 addpopup.addEventListener("click",function(){
     popupoverlay.style.display="block";
     popupbox.style.display="block";
 });
+addnote.addEventListener("click",function(){
+    let title=document.getElementById("note-title");
+    let content=document.getElementById("content");
+    const obj={title:title.value,content:content.value};
+    notes.push(obj);
+});
+
+
 
 
 
